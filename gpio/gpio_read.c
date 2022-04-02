debc197805950150ce107439aabf5f3cac8b30fc
Darshit Agrawal <daag8293@colorado.edu>
2022-04-02 17:32:15 -0600

    Adding libgpiod libraries

---
+++ gpio/gpio_read.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <gpiod.h>
+
+#define GPIO_PIN (4)
+
+struct gpiod_chip *chip;
+struct gpiod_line *line;
+int rv, value;
+
+int main()
+{
+    struct gpiod_chip *chip;
+    struct gpiod_line *line;
+    int req, value;
+
+    chip = gpiod_chip_open("/dev/gpiochip0");
+    if (!chip)
+    {
+        return -1;
+    }
+    line = gpiod_chip_get_line(chip, 3);
+    if (!line) 
+    {
+        gpiod_chip_close(chip);
+	return -1;
+    }
+
+    req = gpiod_line_request_input(line, "gpio_state");
+    if (req) 
+    {
+        gpiod_chip_close(chip);
+	return -1;
+    }
+
+    value = gpiod_line_get_value(line);
+    printf("GPIO value is: %d\n", value);
+    gpiod_chip_close(chip);
+    
+    return 0;
+}
