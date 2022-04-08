b5bf7f5b42613d1b02f4534f250af64a19e252b2
Darshit Agrawal <daag8293@colorado.edu>
2022-04-08 16:56:30 -0600

    Modifying htu21d files

--- humidity/humidity.c
+++ humidity/humidity.c
@@ -3,8 +3,6 @@
  * Author: Darshit Agrawal
  */
 
-#include <stdint.h>
-#include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -14,32 +12,34 @@
 #include <string.h>
 #include <linux/i2c-dev.h>
 #include <linux/i2c.h>
+#include <unistd.h>
+#include <stdint.h>
 
-#define I2C_DEV_PATH 			("/dev/i2c-1")
+#define I2C_DEV_PATH 	("/dev/i2c-1")
 
-int main ()
+int main()
+
 {
-  int fdev = open (I2C_DEV_PATH, O_RDWR);	// open i2c bus
+ int fdev = open("/dev/i2c-1", O_RDWR); // open i2c bus
 
-  if (fdev < 0)
-    {
-      fprintf (stderr, "Failed to open I2C interface %s Error: %s\n", I2C_DEV_PATH, strerror (errno));
-      return -1;
-    }
+if (fdev < 0) {
+    fprintf(stderr, "Failed to open I2C interface %s Error: %s\n", I2C_DEV_PATH, strerror(errno));
+    return -1;
+}
 
-  unsigned char i2c_addr = 0x40;
+unsigned char i2c_addr = 0x40;
 
 // set slave device address 0x40
-  if (ioctl (fdev, I2C_SLAVE, i2c_addr) < 0)
-    {
-      fprintf (stderr, "Failed to select I2C slave device! Error: %s\n", strerror (errno));
-      return -1;
-    }
+if (ioctl(fdev, I2C_SLAVE, i2c_addr) < 0) {
+    fprintf(stderr, "Failed to select I2C slave device! Error: %s\n", strerror(errno));
+    return -1;
+}
 
-  uint8_t buf[1];
-  buf[0] = 0xE3;
+uint8_t buf[1];
+buf[0] = 0xE3;
 
-  write (fdev, buf, 1);
+write(fdev, buf, 1);
+
 
 // device response, 14-bit ADC value:
 //  first 8 bit part ACK  second 8 bit part        CRC
@@ -47,23 +47,21 @@ int main ()
 // bit 15 - measurement type (‘0’: temperature, ‘1’: humidity)
 // bit 16 - currently not assigned
 
-  uint8_t buf1[3] = { 0 };
+uint8_t buf1[3] = { 0 };
 
-  read (fdev, buf1, 3);
+read(fdev, buf1, 3);
 
-  uint16_t sensor_data = (buf1[0] << 8 | buf1[1]) & 0xFFFC;
+uint16_t sensor_data = (buf1 [0] << 8 | buf1 [1]) & 0xFFFC;
 
 // temperature
-  double sensor_tmp = sensor_data / 65536.0;
-  double result = -46.85 + (175.72 * sensor_tmp);
+double sensor_tmp = sensor_data / 65536.0;
+double result = -46.85 + (175.72 * sensor_tmp);
 
-//printf("Temperature: %.2f C\n", result);
+printf("Temperature: %.2f C\n", result);
 
 
 // humidity
-  result = -6.0 + (125.0 * sensor_tmp);
+result = -6.0 + (125.0 * sensor_tmp);
 
-  printf ("Humidity: %.2f %%\n", result);
-
-  return 0;
+printf("Humidity: %.2f %%\n", result);
 }
